91d77b13d9c14615da6b0937c8a7412f461cca11
Chenyue.Tao <3038816978@qq.com>
2022-07-05 10:13:28 +0800

    update

--- README.md
+++ README.md
@@ -4,7 +4,5 @@ Some algorithms in deep-learning. Realizing and discussing them.
 ## Reference 
 The link of related papers will be collect in directory (references). Some related information will also be collected.
 
-## Code
-In directory code, some algorithms will be realized. Code with similarity should avoid being rewritten.
 
 
---
+++ make_datasets/make_MNIST.py
@@ -0,0 +1,24 @@
+from sqlalchemy import desc
+from torchvision.datasets import MNIST
+from torchvision import transforms
+import cv2
+import os
+import numpy as np
+from tqdm import tqdm
+
+root='datasets/'
+subffix='.png'
+
+trans=transforms.Compose([transforms.ToTensor()])
+trainset=MNIST(root,train=True,transform=trans,download=True)
+testset=MNIST(root,train=False,transform=trans,download=True)
+def make_image(directory,set):
+    if not os.path.exists(root+directory):
+        os.makedirs(root+directory)
+    for i,item in tqdm(enumerate(set),desc=directory):
+        image=np.uint8(item[0].numpy()[0]*255)
+        cv2.imwrite(root+directory+str(i)+subffix,image)
+
+make_image('MNIST/images/train/',trainset)
+make_image('MNIST/images/test/',testset)
+    
